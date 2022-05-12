de6dee1a93b2726a2dc1a2a2ce8d12378abdf092
Pacifist <desmondmorales92@gmail.com>
2022-05-12 17:06:05 +0300

    this calculator

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+"use strict"
+
+// let calculator = {
+//     read() {
+//         this.a = +prompt("Enter first value")
+//         if (isNaN(this.a)) {
+//             console.log("You didn't enter number")
+//         }   else {
+//             this.b = +prompt("Enter second value")
+//             if (isNaN(this.b)) {
+//                 console.log("You didn't enter number")
+//             }
+//         }
+//     },
+//     sum() {
+//         let result = this.a + this.b
+//         return result
+//     },
+//     mul() {
+//         let result = this.a * this.b
+//         return result
+//     }
+// }
+
+let calculator = {
+    read() {
+        this.a = +prompt("Enter first value")
+        if (isNaN(this.a)) {
+            console.log("You didn't enter number")
+        }   else {
+            this.b = +prompt("Enter second value")
+            if (isNaN(this.b)) {
+                console.log("You didn't enter number")
+            }
+        }
+    },
+    sum() {
+        let result = this.a + this.b
+        return result
+    },
+    mul() {
+        let result = this.a * this.b
+        return result
+    }
+}
+
+calculator.read();
+console.log(calculator.sum())
+console.log(calculator.mul());
